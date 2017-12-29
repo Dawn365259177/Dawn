4c2eb7556582fbe546bb16f4d21e731ef3732934
hy <365259177@qq.com>
2017-12-30 01:19:29 +0800

    the first times

---
+++ src/main/java/cn/wolfcode/wms/App.java
@@ -0,0 +1,13 @@
+package cn.wolfcode.wms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
